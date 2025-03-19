cbd7b9bf0e0a7ba9c6d43ccce7f2a39347c9a313
Heikal Syah Shiddiq <heikalsyahshiddiq@gmail.com>
2025-03-20 03:51:49 +0700

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,29 @@
-import { Elysia } from "elysia";
+import { Elysia } from 'elysia';
+import NekoBocc from 'nekobocc';
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const nekobocc = new NekoBocc();
+const app = new Elysia().get('/', 'Server is online');
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+const getRandom = async () => {
+  return await nekobocc.random();
+};
+
+const getHentai = async (url: string) => {
+  return await nekobocc.get(url);
+};
+
+const getRelease = async (page: number) => {
+  return await nekobocc.release(page);
+};
+
+const getSearch = async (query: string) => {
+  return await nekobocc.search(query);
+};
+
+app.get('/random', getRandom);
+app.get('/get', ({ query }) => getHentai(query.url));
+app.get('/release', ({ query }) => getRelease(Number(query.page)));
+app.get('/search', ({ query }) => getSearch(query.q));
+
+app.listen(3000)
+console.log(`Server is running at ${app.server?.hostname}:${app.server?.port}`);
